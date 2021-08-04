aa640c54fb0ea2772f48a35fb07052f3b8895721
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-08-04 09:39:21 +0430

    pro_shop_app_api

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$server="localhost";
+$user="root";
+$pass="";
+$dbname="divar";
+
+$dsn="mysql:host=$server;dbname=$dbname";
+try{
+    $connection=new PDO($dsn,$user,$pass);
+    $connection->exec("SET character_set_connection='utf8'");
+    $connection->exec("SET NAMES 'UTF8'");
+
+}catch(PDOException $error){
+    echo "unable to connect ".$error->getMessage();
+}
